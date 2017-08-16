bf144a4880300704d2109d4cbf5815ccf71e169c
Sparrow.jang <sparrow.jang@gmail.com>
2017-08-16 16:16:00 +0800

    Implement pencil button of black and blue colors

---
+++ src/js/components/BlackDrawingButton.js
@@ -0,0 +1,9 @@
+
+import CustomizeDrawingButton from './CustomizeDrawingButton';
+
+let BlackDrawingButton = CustomizeDrawingButton.extend({
+  name :'blackDrawing'
+});
+
+export default BlackDrawingButton;
+
---
+++ src/js/components/BlueDrawingButton.js
@@ -0,0 +1,10 @@
+
+import CustomizeDrawingButton from './CustomizeDrawingButton';
+
+let BlueDrawingButton = CustomizeDrawingButton.extend({
+  name :'blueDrawing',
+  color:'rgba(0, 0, 255, 1)'
+});
+
+export default BlueDrawingButton;
+
